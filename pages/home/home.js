ae6aa1fa936b02b911e46bb421c99e3be287d47c
iamyishan <872644669@qq.com>
2021-04-13 16:13:05 +0800

    事件处理详情

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,45 +5,61 @@ Page({
    * 页面的初始数据
    */
   data: {
-    name: "yishan",
-    age: 18,
-    students: [{
-        id: 110,
-        name: "kobe",
-        age: 30
-      },
-      {
-        id: 111,
-        name: "james",
-        age: 28
-      },
-      {
-        id: 112,
-        name: "curyy",
-        age: 32
-      },
-      {
-        id: 113,
-        name: "yishan",
-        age: 18
-      }
-    ],
-    counter: 0
+    titles: ['衣服', '裤子', '鞋子']
   },
   handleBtnClick() {
-    // 1.错误做法：界面是不会刷新的
-    // this.data.counter += 1
-    // console.log(this.data.counter)
-
-    // 2.this.setData()
-    this.setData({
-      counter: this.data.counter + 1
-    })
+    console.log('按钮发送点击')
   },
-  handleSubtraction() {
-    this.setData({
-      counter: this.data.counter - 1
-    })
+  handleTouchStart() {
+    console.log('handleTouchStart')
+  },
+  handleTouchMove() {
+    console.log('handleTouchMove')
+  },
+  handleTouchEnd() {
+    console.log("handleTouchEnd")
+  },
+  handleTap() {
+    console.log('handleTap')
+  },
+  handleLongpress() {
+    console.log("handleLongpress")
+  },
+  handleEventClick(event) {
+    console.log("-------", event)
+  },
+  handleEventEnd(event) {
+    console.log('+++++++', event)
+  },
+  handleInner(event) {
+    console.log(event)
+  },
+  handleOuter(event) {
+    console.log(event)
+  },
+  handleItemClick(event) {
+    console.log(event.currentTarget.dataset)
+  },
+  
+  // ----------  事件冒泡和事件捕获
+  handleCaptureView1() {
+    console.log('handleCaptureView1')
+  },
+  handleBindView1() {
+    console.log('handleBindView1')
+  },
+  handleCaptureView2() {
+    console.log('handleCaptureView2')
+  },
+  handleBindView2() {
+    console.log('handleBindView2')
+  },
+  handleCaptureView3() {
+    console.log('handleCaptureView3')
+  },
+  handleBindView3() {
+    console.log('handleBindView3')
   }
+
 
 })
