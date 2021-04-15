27383384f0e9fcbcbc95e197146b2c5ef2670903
iamyishan <872644669@qq.com>
2021-04-15 16:11:31 +0800

    弹窗和分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,49 +1,67 @@
 // pages/home/home.js
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    name: "yishan",
-    age: 18,
-    students: [{
-        id: 110,
-        name: "kobe",
-        age: 30
+  handleShowToast() {
+    wx.showToast({
+      title: '加载中ing',
+      duration: 3000,
+      icon: 'loading',
+      mask: true,
+      success: function () {
+        console.log('展示弹窗成功')
       },
-      {
-        id: 111,
-        name: "james",
-        age: 28
+      fail: function () {
+        console.log('展示弹窗失败')
       },
-      {
-        id: 112,
-        name: "curyy",
-        age: 32
-      },
-      {
-        id: 113,
-        name: "yishan",
-        age: 18
+      complete: function () {
+        console.log('完成函数的调用')
       }
-    ],
-    counter: 0
-  },
-  handleBtnClick() {
-    // 1.错误做法：界面是不会刷新的
-    // this.data.counter += 1
-    // console.log(this.data.counter)
-
-    // 2.this.setData()
-    this.setData({
-      counter: this.data.counter + 1
     })
   },
-  handleSubtraction() {
-    this.setData({
-      counter: this.data.counter - 1
+  handleShowModal() {
+    wx.showModal({
+      title: "我是标题",
+      content: "我是内容",
+      cancelColor: 'cancelColor',
+      cancelText: '退出',
+      cancelColor: 'red',
+      success: function (res) {
+        console.log(res)
+        if (res.cancel) {
+          console.log('用户点击了取消按钮')
+        }
+        if (res.confirm) {
+          console.log('用户点击了确定按钮')
+        }
+      }
     })
+  },
+  handleLoading() {
+    wx.showLoading({
+      title: '加载ing',
+      mask: true
+    })
+    // 必须手动hideLoading才会让loading消失
+    setTimeout(
+      res => {
+        wx.hideLoading()
+      }, 1000
+    )
+  },
+  handleShowAction() {
+    wx.showActionSheet({
+      itemList: ['相册', '拍照'],
+      itemColor: 'red',
+      success: function (res) {
+        console.log(res)
+      }
+    })
+  },
+  onShareAppMessage: function (options) {
+    return {
+      title: "hello yishan",
+      path: '/pages/about/about',
+      imageUrl: 'http://s3.mogucdn.com/mlcdn/c45406/170915_0a93207ci28kelh617k4hh62l65lb_640x960.jpg'
+    }
   }
-
 })
